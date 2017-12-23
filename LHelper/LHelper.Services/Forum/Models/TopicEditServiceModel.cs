9a7d5894be8584913a95110891bd78f4acaf3853
KameliyaIvanova <iv.kameliya@gmail.com>
2017-12-23 15:18:32 +0200

    ErrorInTopicService

---
+++ LHelper/LHelper.Services/Forum/Models/TopicEditServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LHelper.Services.Forum.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class TopicEditServiceModel
+    {
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        
+    }
+}
